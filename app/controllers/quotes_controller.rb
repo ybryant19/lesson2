2f295a0e12626f080c7f0b168712c6833c9be466
Yvette Bryant <bryant.yvette@gmail.com>
2019-09-16 16:36:08 +0000

    added about section with bio and pic

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -9,11 +9,14 @@ def new
   end
 
 def create
-  Quote.create(quote_params)
+  @quote = Quote.create(quote_params)
   if @quote.invalid?
     flash[:error] = '<strong>Could not save</strong> the data you entered is invalid.'
   end  
   redirect_to root_path
+end
+
+def about
 end
 
 private
